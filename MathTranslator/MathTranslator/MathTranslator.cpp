c8100649a6b2bfdb3ba3ad9b83a733ea9e499fe9
Arseny Astashkin <ars.astashkin@gmail.com>
2014-09-16 15:55:53 +0400

    Добавлено чтение данных из файла.

--- MathTranslator/MathTranslator/MathTranslator.cpp
+++ MathTranslator/MathTranslator/MathTranslator.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void wMTRead(wchar_t* inputFileName, wchar_t* input) {
+	
+
+	std::wfstream someFile;
+	someFile.open(inputFileName);
+	someFile >> input;
+	std::wcout << input << std::endl;
+	someFile.close();
+};
+
+extern void Translate();
+extern void Write();
 
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int wmain(int argc, wchar_t* argv[]) {
+
+	wchar_t* buffer = new wchar_t[1024];
+	wMTRead(argv[1], buffer);
+
 	return 0;
 }
 
