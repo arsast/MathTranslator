530da24f53efc4a4c6703ffb33e0157b833a86b1
Levshin Nikolay <levshin.nikolay@phystech.edu>
2014-10-06 14:49:04 +0400

    Убрал Хинди-Код. Добавлены неравенства.

--- MathTranslator/MathTranslator/ConvertOM.cpp
+++ MathTranslator/MathTranslator/ConvertOM.cpp
@@ -7,13 +7,11 @@
 #include <string>
 #include <sstream>
 #include <fstream>
-#include <map>
 
-typedef std::map<std::string, std::pair<std::string, int>> IdCollection;
 
 void LoadIdTable( IdCollection* ids )
 {
-    std::ifstream fin( "OMTable.txt" ); //  в данном файле содержитс€ таблица соответстви€ идентификаторов операндам
+    std::ifstream fin( "OMTable.txt" ); //  в данном файле содержитс€ таблица соответстви€ атрибутов операндов идентификаторам
     std::string type, contDict, name;
     int id;
     while( fin ) // считываем файл и заполн€ем контейнер данными из таблицы
@@ -21,7 +19,7 @@ void LoadIdTable( IdCollection* ids )
         fin >> id >> type >> contDict >> name;
         if( fin )
         {
-            if( name == "times" )
+            if( name == "times" ) // так как умножени€ два, то пока что такой костыль.
             {
                 if( contDict == "arith1" )
                 {
@@ -40,7 +38,21 @@ void LoadIdTable( IdCollection* ids )
     }
     fin.close( );
 }
-
+void LoadAttrTable( AttrCollection* attrs )
+{
+    std::ifstream fin( "OMTable.txt" ); //  в данном файле содержитс€ таблица соответстви€ атрибутов операндов идентификаторам
+    std::string type, contDict, name;
+    int id;
+    while( fin ) // считываем файл и заполн€ем контейнер данными из таблицы
+    {
+        fin >> id >> type >> contDict >> name;
+        if( fin )
+        {
+            ( *attrs )[id] = std::make_pair( contDict, name );
+        }
+    }
+    fin.close( );
+}
 void ConvertFromOM( char* inputFileName, MathObj* obj ) 
 {
    
@@ -57,7 +69,6 @@ void ConvertFromOM( char* inputFileName, MathObj* obj )
 
 	ConvertElemToObj(&ids, pElem, obj );
 }
-
 void ConvertElemToObj( IdCollection* ids, TiXmlElement* pElem, MathObj* obj ) {
 	for( pElem; pElem; pElem = pElem->NextSiblingElement() ) //пробегаемс€ по всем элементам одного сло€
 	{
@@ -75,14 +86,14 @@ void ConvertElemToObj( IdCollection* ids, TiXmlElement* pElem, MathObj* obj ) {
 		{
 			TiXmlAttribute* pAttrib = pElem->FirstAttribute();
 			ParamObj* node = new ParamObj( pAttrib->Value() );
-			((FormulaObj*)obj)->params.push_back( node );
+            static_cast<FormulaObj*>( obj )->params.push_back( node );
 		}
 
 		//≈сли целое число
 		else if( pKey == "OMI" ) 
 		{
 			ParamObj* node = new ParamObj( pElem->GetText() );
-			((FormulaObj*)obj)->params.push_back( node );
+            static_cast<FormulaObj*>( obj )->params.push_back( node );
 		}
 
         //≈сли дес€тичное число
@@ -90,14 +101,14 @@ void ConvertElemToObj( IdCollection* ids, TiXmlElement* pElem, MathObj* obj ) {
         {
             TiXmlAttribute* pAttrib = pElem->FirstAttribute();
             ParamObj* node = new ParamObj( pAttrib->Value( ) );
-            ( (FormulaObj*) obj )->params.push_back( node );
+            static_cast<FormulaObj*>( obj )->params.push_back( node );
 
         }
 		//≈сли конструкци€, содержаща€ массив операций
 		else if( pKey == "OMA" ) 
 		{
 			FormulaObj* node = new FormulaObj();
-			((FormulaObj*)obj)->params.push_back( node );
+            static_cast<FormulaObj*>( obj )->params.push_back( node );
 			ConvertElemToObj( ids, pElem->FirstChildElement(), node );
 		}
 
@@ -124,12 +135,11 @@ void ConvertElemToObj( IdCollection* ids, TiXmlElement* pElem, MathObj* obj ) {
 			}
 
 			std::string operand = pAttrib->Value(); // ѕолучаем тип операнда
-			SetFormulaObjType( ids, (FormulaObj*)obj, &cd, &operand );
+            SetFormulaObjType( ids, static_cast<FormulaObj*>( obj ), &cd, &operand );
 		}
 	}
 
 }
-
 void ConvertToOM( char* outputFileName, MathObj* obj ) 
 {
 	TiXmlDocument doc;
@@ -137,12 +147,13 @@ void ConvertToOM( char* outputFileName, MathObj* obj )
 	element->SetAttribute( "xmlns", "http://www.openmath.org/OpenMath" );
 	doc.LinkEndChild( element );
 
-	ConvertObjToElem( element, ((FormulaObj*)obj)->params[0] );
+    AttrCollection attrs;
+    LoadAttrTable( &attrs );
+    ConvertObjToElem( &attrs, element, static_cast<FormulaObj*>( obj )->params[0] );
 
     doc.SaveFile( outputFileName );
 }
-
-void ConvertObjToElem( TiXmlElement* pElem, MathObj* obj ) 
+void ConvertObjToElem( AttrCollection* attrs, TiXmlElement* pElem, MathObj* obj ) 
 {
 	std::string objType = typeid( *obj ).name();
 	std::cout << objType.c_str() << std::endl;
@@ -153,14 +164,14 @@ void ConvertObjToElem( TiXmlElement* pElem, MathObj* obj )
 
 		TiXmlElement* formulaElement = new TiXmlElement( "OMS" );
 
-		SetFormulaElementAttribute( formulaElement, ((FormulaObj*)obj)->GetType() );
+		SetFormulaElementAttribute( attrs, formulaElement, ((FormulaObj*)obj)->GetType() );
 		element->LinkEndChild(formulaElement);
 
 		std::vector<MathObj*>::iterator it = ((FormulaObj*)obj)->params.begin();
 		std::vector<MathObj*>::const_iterator itEnd = ((FormulaObj*)obj)->params.end();
 		for( it; it != itEnd; ++it ) 
 		{
-			ConvertObjToElem( element, *it );
+			ConvertObjToElem( attrs, element, *it );
 		}
     }
     else if( objType == "class ParamObj" )
@@ -190,183 +201,21 @@ void ConvertObjToElem( TiXmlElement* pElem, MathObj* obj )
         }
     }
 }
-
-void SetFormulaElementAttribute( TiXmlElement* pElem, TNodeType type ) 
+void SetFormulaElementAttribute( AttrCollection* attrs, TiXmlElement* pElem, TNodeType type ) 
 {
-	switch ( type )
-	{
-	case NT_UMINUS:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "unary_minus" );
-		break;
-	case NT_PLUS:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "plus" );
-		break;
-	case NT_MINUS:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "minus" );
-		break;
-	case NT_EQUAL:
-		pElem->SetAttribute( "cd", "relation1" );
-		pElem->SetAttribute( "name", "eq" );
-		break;
-	case NT_MULTNCM:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "times" );
-		break;
-    case NT_MULTCM:
-        pElem->SetAttribute( "cd", "arith2" );
-        pElem->SetAttribute( "name", "times" );
-        break;
-	case NT_DIV:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "divide" );
-		break;
-	case NT_ROOT:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "root" );
-		break;
-	case NT_POW:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "power" );
-		break;
-	case NT_ABS:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "abs" );
-		break;
-	case NT_SUM:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "sum" );
-		break;
-	case NT_PROD:
-		pElem->SetAttribute( "cd", "arith1" );
-		pElem->SetAttribute( "name", "product" );
-		break;
-    case NT_INVERSE:
-        pElem->SetAttribute( "cd", "arith2" );
-        pElem->SetAttribute( "name", "inverse" );
-        break;
-    case NT_LOG:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "log" );
-        break;
-    case NT_LN:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "ln" );
-        break;
-    case NT_EXP:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "exp" );
-        break;
-    case NT_SIN:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "sin" );
-        break;
-    case NT_COS:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "cos" );
-        break;
-    case NT_SEC:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "sec" );
-        break;
-    case NT_CSC:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "csc" );
-        break;
-    case NT_TAN:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "tan" );
-        break;
-    case NT_COT:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "cot" );
-        break;
-    case NT_SINH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "sinh" );
-        break;
-    case NT_COSH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "cosh" );
-        break;
-    case NT_SECH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "SECH" );
-        break;
-    case NT_CSCH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "csch" );
-        break;
-    case NT_TANH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "tanh" );
-        break;
-    case NT_COTH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "coth" );
-        break;
-    case NT_ARCSIN:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arcsin" );
-        break;
-    case NT_ARCCOS:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccos" );
-        break;
-    case NT_ARCSEC:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arcsec" );
-        break;
-    case NT_ARCCSC:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccsc" );
-        break;
-    case NT_ARCTAN:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arctan" );
-        break;
-    case NT_ARCCOT:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccot" );
-        break;
-    case NT_ARCSINH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arcsinh" );
-        break;
-    case NT_ARCCOSH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccosh" );
-        break;
-    case NT_ARCSECH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arcsech" );
-        break;
-    case NT_ARCCSCH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccsch" );
-        break;
-    case NT_ARCTANH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arctan" );
-        break;
-    case NT_ARCCOTH:
-        pElem->SetAttribute( "cd", "transc1" );
-        pElem->SetAttribute( "name", "arccoth" );
-        break;
-    case NT_PLUSMINUS:
-        pElem->SetAttribute( "cd", "multiops" );
-        pElem->SetAttribute( "name", "plusminus" );
-        break;
-	default:
+    if( attrs->find( static_cast<int>( type ) ) != attrs->end() )
+    {
+        pElem->SetAttribute( "cd", (*attrs)[type].first.c_str() );
+        pElem->SetAttribute( "name", (*attrs)[type].second.c_str() );
+    }
+    else
+    {
 		std::cerr << "Error: invalid type of Node in MathObj:" << type << std::endl;
 	}
 }
-
 void SetFormulaObjType( IdCollection* ids, FormulaObj* obj, std::string* cd, std::string* operand)
 {
-    if( *operand == "times" )
+    if( *operand == "times" )// так как умножени€ два, то такой вот костыль, пару к нему ищи в функции LoadIdTable
     {
         if( *cd == "arith1" )
         {
--- MathTranslator/MathTranslator/ConvertOM.h
+++ MathTranslator/MathTranslator/ConvertOM.h
@@ -5,11 +5,13 @@
 #include <map>
 
 typedef std::map<std::string, std::pair<std::string, int>> IdCollection;
+typedef std::map<int, std::pair<std::string, std::string>> AttrCollection;
 
 void ConvertFromOM( char*, MathObj* ); // Конвертирует OpenMath в MathObj, принимая на вход имя файла, откуда читаем
 void ConvertElemToObj( IdCollection*, TiXmlElement*, MathObj* ); // Конвертирует в MathObj, принимая на вход указатель на текущий элемент DOM
 void ConvertToOM( char*, MathObj* ); // Конвертирует MathObj в OpenMath, принимая на вход имя файла, в который записываем
-void ConvertObjToElem( TiXmlElement*, MathObj* ); // Конвертирует в DOM, принимая на вход указатель на текущий элемент MathObj
-void SetFormulaElementAttribute( TiXmlElement*, const TNodeType ); // Устанавливает элементу DOM нужные атрибуты
+void ConvertObjToElem( AttrCollection*, TiXmlElement*, MathObj* ); // Конвертирует в DOM, принимая на вход указатель на текущий элемент MathObj
+void SetFormulaElementAttribute( AttrCollection*, TiXmlElement*, const TNodeType ); // Устанавливает элементу DOM нужные атрибуты
 void SetFormulaObjType( IdCollection*, FormulaObj*, std::string*, std::string* ); // Устанавливает элементу FormulaObj нужный флаг в зависимости от атрибутов 
 void LoadIdTable( IdCollection* ); // Подгружает таблицу операторов
+void LoadAttrTable( AttrCollection* ); // Подгружает таблицу атрибутов
