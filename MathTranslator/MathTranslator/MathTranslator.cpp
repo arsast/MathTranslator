0a85ba516b5cdfc4e28c13696a2b67b03c4c3c93
IgorStolbov <igor.stolbov@yandex.ru>
2014-10-06 16:40:00 +0400

    Убрал свои тесты из MathTranslator

--- MathTranslator/MathTranslator/MathTranslator.cpp
+++ MathTranslator/MathTranslator/MathTranslator.cpp
@@ -31,17 +31,15 @@ void MTWrite( char* param, char* outputFileName, MathObj* obj )
 
 int main( int argc, char* argv[] ) 
 {
-	//if( argc != 5 ) {
-	//	std::cout << "Nevernoe kolichestvo parametrov!" << std::endl;
-	//	system( "pause" );
-	//	return 0;
-	//}
-	//
-	//FormulaObj obj( NT_MAIN );
-	//MTRead( argv[1], argv[2], &obj );
-	//MTWrite( argv[3], argv[4], &obj );
-	MathMLParser A;
-	A.Pars("inputMML.txt");
+	if( argc != 5 ) {
+		std::cout << "Nevernoe kolichestvo parametrov!" << std::endl;
+		system( "pause" );
+		return 0;
+	}
+	
+	FormulaObj obj( NT_MAIN );
+	MTRead( argv[1], argv[2], &obj );
+	MTWrite( argv[3], argv[4], &obj );
 
 	system( "pause" );
 	return 0;
