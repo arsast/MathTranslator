7504c21610643e44634b8ec41abfcdc5d465d57b
Levshin Nikolay <levshin.nikolay@phystech.edu>
2014-10-14 12:55:29 +0400

    100% рабочая версия
    прошлая "рабочая" оказалась не рабочей

--- MathTranslator/MathTranslator/MathTranslator.cpp
+++ MathTranslator/MathTranslator/MathTranslator.cpp
@@ -9,76 +9,76 @@
 #include "ConvertOM.h"
 #include "MathML.h"
 
-//void MTConvert( char* inputParam, char* inputFileName, char* outputParam, char* outputFileName )
-//{
-//	FormulaObj obj( NT_MAIN );
-//	MathMLParser mmlparser;
-//
-//	std::string inputPar( inputParam );
-//
-//	if( inputPar == "omath" )
-//	{
-//		ConvertFromOM( inputFileName, &obj );
-//	}
-//	if( inputPar == "mathml" )
-//	{
-//		mmlparser.Pars( inputFileName );
-//	}
-//	std::string outputPar( outputParam );
-//	if( outputPar == "omath" )
-//	{
-//		if( inputPar == "omath" )
-//		{
-//			ConvertToOM( outputFileName, &obj );
-//		}
-//		if( inputPar == "mathml" )
-//		{
-//			ConvertToOM( outputFileName, mmlparser.GetData() );
-//		}
-//	}
-//    if( outputPar == "mathml" )
-//    {
-//        if( inputPar == "omath" )
-//        {
-//            mmlparser.SetData( &obj );
-//            mmlparser.Save( outputFileName );
-//        }
-//        if( inputPar == "mathml" )
-//            mmlparser.Save( outputFileName );
-//    }
-//}
-
-void MTRead( char* param, char* inputFileName, MathObj* obj ) 
+void MTConvert( char* inputParam, char* inputFileName, char* outputParam, char* outputFileName )
 {
-	std::string par( param );
-	if( par == "omath" ) 
-    {
-		ConvertFromOM( inputFileName, obj );
-	}
-	if( par == "mathml" )
+	FormulaObj obj( NT_MAIN );
+	MathMLParser mmlparser;
+
+	std::string inputPar( inputParam );
+
+	if( inputPar == "omath" )
 	{
-		MathMLParser* A = new MathMLParser();
-		A->Pars( inputFileName );
-		obj = A->GetData();
+		ConvertFromOM( inputFileName, &obj );
 	}
-	
-}
-
-void MTWrite( char* param, char* outputFileName, MathObj* obj ) 
-{
-	std::string par( param );
-	if( par == "omath" ) 
-    {
-		ConvertToOM( outputFileName, obj );
+	if( inputPar == "mathml" )
+	{
+		mmlparser.Pars( inputFileName );
 	}
-
-    if( par == "mathml" )
+	std::string outputPar( outputParam );
+	if( outputPar == "omath" )
+	{
+		if( inputPar == "omath" )
+		{
+			ConvertToOM( outputFileName, &obj );
+		}
+		if( inputPar == "mathml" )
+		{
+			ConvertToOM( outputFileName, mmlparser.GetData() );
+		}
+	}
+    if( outputPar == "mathml" )
     {
-        MathMLParser* A = new MathMLParser();
-        A->SetData( obj );
-        A->Save( outputFileName );
+        if( inputPar == "omath" )
+        {
+            mmlparser.SetData( &obj );
+            mmlparser.Save( outputFileName );
+        }
+        if( inputPar == "mathml" )
+            mmlparser.Save( outputFileName );
     }
 }
+
+//void MTRead( char* param, char* inputFileName, MathObj* obj ) 
+//{
+//	std::string par( param );
+//	if( par == "omath" ) 
+//    {
+//		ConvertFromOM( inputFileName, obj );
+//	}
+//	if( par == "mathml" )
+//	{
+//		MathMLParser* A = new MathMLParser();
+//		A->Pars( inputFileName );
+//		obj = A->GetData();
+//	}
+//	
+//}
+
+//void MTWrite( char* param, char* outputFileName, MathObj* obj ) 
+//{
+//	std::string par( param );
+//	if( par == "omath" ) 
+//    {
+//		ConvertToOM( outputFileName, obj );
+//	}
+//
+//    if( par == "mathml" )
+//    {
+//        MathMLParser* A = new MathMLParser();
+//        A->SetData( obj );
+//        A->Save( outputFileName );
+//    }
+//}
 
 
 int main( int argc, char* argv[] ) 
@@ -90,10 +90,10 @@ int main( int argc, char* argv[] )
 	}
 
     FormulaObj obj( NT_MAIN );
-    MTRead( argv[1], argv[2], &obj);
+    /*MTRead( argv[1], argv[2], &obj);
     MTWrite( argv[3], argv[4], &obj );
-
-    //MTConvert( argv[1], argv[2], argv[3], argv[4] );
+*/
+    MTConvert( argv[1], argv[2], argv[3], argv[4] );
 
 	system( "pause" );
 	return 0;
